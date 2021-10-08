efc9eae1515765d12485aff70bbcc848ccb36f15
Dominic McGinty <McGinty.Dom@gmail.com>
2021-10-08 13:05:47 +0100

    Path - Return dummy values

--- src/com/company/Path.java
+++ src/com/company/Path.java
@@ -1,83 +1,85 @@
 package com.company;
 
 public class Path {
-
+    private static final int DUMMY_INT = 0;
+    private static final boolean DUMMY_BOOL = false;
+    
     //UNIQUE PATHS
 
     //Medium - https://leetcode.com/problems/unique-paths/
     public int uniquePaths(int m, int n) {
-
+        return DUMMY_INT;
     }
 
     //Medium - https://leetcode.com/problems/unique-paths-ii/
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-
+        return DUMMY_INT;
     }
 
     //Hard - https://leetcode.com/problems/unique-paths-iii/
     public int uniquePathsIII(int[][] grid) {
-
+        return DUMMY_INT;
     }
 
     //SHORTEST PATHS
 
     //Medium - https://leetcode.com/problems/shortest-path-in-binary-matrix/
     public int shortestPathBinaryMatrix(int[][] grid) {
-
+        return DUMMY_INT;
     }
 
     //Medium - https://leetcode.com/problems/minimum-path-sum/
     public int minPathSum(int[][] grid) {
-
+        return DUMMY_INT;
     }
 
     //Medium - https://leetcode.com/problems/path-with-minimum-effort/
     public int minimumEffortPath(int[][] heights) {
-
+        return DUMMY_INT;
     }
 
     //Hard - https://leetcode.com/problems/shortest-path-to-get-all-keys/
     public int shortestPathAllKeys(String[] grid) {
-
+        return DUMMY_INT;
     }
 
     //Hard - https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
     public int shortestPath(int[][] grid, int k) {
-
+        return DUMMY_INT;
     }
 
     //LONGEST PATHS
 
     //Medium - https://leetcode.com/problems/path-with-maximum-gold/
     public int getMaximumGold(int[][] grid) {
-
+        return DUMMY_INT;
     }
 
     //Hard - https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
     public int longestIncreasingPath(int[][] matrix) {
-
+        return DUMMY_INT;
     }
 
     //VALID PATHS
 
     //Easy - https://leetcode.com/problems/path-crossing/
     public boolean isPathCrossing(String path) {
-
+        return DUMMY_BOOL;
     }
 
     //Easy - https://leetcode.com/problems/find-if-path-exists-in-graph/
     public boolean validPath(int n, int[][] edges, int start, int end) {
-
+        return DUMMY_BOOL;
     }
 
     //Medium - https://leetcode.com/problems/check-if-there-is-a-valid-path-in-a-grid/
     public boolean hasValidPath(int[][] grid) {
-
+        return DUMMY_BOOL;
     }
 
     //Medium - https://leetcode.com/problems/path-with-maximum-probability/
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-
+        return (double) DUMMY_INT;
     }
 
 }
